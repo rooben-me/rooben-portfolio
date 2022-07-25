8a28f13162d7f3f803f5c91e4f078b87b25b3f6b
rooben-me <rubankumar.nkl@gmail.com>
2022-07-25 15:36:30 +0530

    removed dark mode for the time being

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,17 +20,17 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="grid grid-cols-2 md:grid-cols-3">
+    <section className="grid grid-cols-2 md:grid-cols-3">
       <Image
         src={roobenlogo}
-        alt="rooben lovin logo"
+        alt="logo of rooben"
         layout="fixed"
         width={160}
         height={24}
         className="place-self-start"
       />
 
-      <div className="hidden md:flex md:gap-16 md:mx-auto md:items-center">
+      <nav className="hidden md:flex md:gap-16 md:mx-auto md:items-center">
         {navItems.map(({ name, href }) => (
           <Link key={name} href={href}>
             <a className="text-link text-lg hover:text-primary-main transition-colors ease-in-out duration-500">
@@ -38,14 +38,14 @@ const Navbar = () => {
             </a>
           </Link>
         ))}
-      </div>
+      </nav>
 
       <Link href="#">
         <a className="primary-gradient text-xl place-self-end font-medium">
           Contact me
         </a>
       </Link>
-    </nav>
+    </section>
   );
 };
 

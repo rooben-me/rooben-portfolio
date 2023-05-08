5d31719f4e6e0e7a9898b416e638d01ffa24060c
rooben-me <rubankumar.nkl@gmail.com>
2023-05-09 01:26:56 +0530

    fixed png image

--- utils/mdxUtils.ts
+++ utils/mdxUtils.ts
@@ -81,7 +81,7 @@ export function getAllPosts(fields: string[]): Items[] {
   // get the posts from the filepaths with the needed fields sorted by date
   const posts = filePaths
     .map((filePath) => getPostItems(filePath, fields))
-    .sort((post1, post2) => (post1.date > post2.date ? 1 : -1));
+    .sort((post1, post2) => (post1.date < post2.date ? 1 : -1));
   // return the available post
   return posts;
 }

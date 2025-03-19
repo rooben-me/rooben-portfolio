7e98c287c8cbafa352206560d890faca05c91bc3
ruban <51721541+rooben-me@users.noreply.github.com>
2025-03-19 20:17:49 +0000

    Add posthog analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,36 @@
+import { useEffect } from "react";
+import type { AppProps } from "next/app";
+import { NextSeo } from "next-seo";
+import { Router } from 'next/router'
+
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
 import "../styles/globals.css";
 import "../styles/prism-coldark-dark.css";
 
-import type { AppProps } from "next/app";
-import { NextSeo } from "next-seo";
-
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      // Enable debug mode in development
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug()
+      }
+    })
+
+    const handleRouteChange = () => posthog?.capture('$pageview')
+
+    Router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    }
+  }, [])
+
+
   return (
     <>
       <NextSeo
@@ -27,8 +53,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           ],
         }}
       />
-
-      <Component {...pageProps} />
+      <PostHogProvider client={posthog}>
+        <Component {...pageProps} />
+      </PostHogProvider>
     </>
   );
 }
